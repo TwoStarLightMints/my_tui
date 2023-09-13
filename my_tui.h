cdd0f11ba050a2513f5ae15eb14687d48a89a5a2
known <mondo.beyondo3@gmail.com>
2023-09-12 20:58:17 -0400

    Implement class

--- my_tui.h
+++ my_tui.h
@@ -1,4 +1,19 @@
 #ifndef MY_TUI
 #define MY_TUI
+#include <vector>
+#include <string>
+#include <iostream>
+
+class Menu {
+    public:
+        Menu() {}
+        void print_menu();
+        void add_option(std::string new_opt);
+        void set_focus(int direction);
+    
+    private:
+        std::vector<std::string> options;
+        int focus = 0;
+};
 
 #endif
