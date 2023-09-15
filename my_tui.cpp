324622c07799dfed9fcd9d5bd8af78b8feec4ffb
known <mondo.beyondo3@gmail.com>
2023-09-15 12:30:13 -0400

    Implement get_focus for linux systems and custom_pause for linux pausing

--- my_tui.cpp
+++ my_tui.cpp
@@ -1,5 +1,10 @@
 #include "my_tui.h"
+#include <termios.h>
 
+void custom_pause() {
+    std::cout << "Press enter to continue...";
+    std::cin.get();
+}
 
 /*================================================================================================================*/
 //
@@ -10,7 +15,7 @@
 void Menu::render_menu() { // TODO: Hide cursor on Linux machines
     int cont;
     
-    #ifdef _WIN32 // Hide cursor on Windows
+    #ifdef _WIN32 // Hide cursor on Windows, found in https://stackoverflow.com/questions/18028808/remove-blinking-underscore-on-console-cmd-prompt by Captain Oblivious
     HANDLE out = GetStdHandle(STD_OUTPUT_HANDLE);
     CONSOLE_CURSOR_INFO cursor_info;
     GetConsoleCursorInfo(out, &cursor_info);
@@ -88,6 +93,28 @@ void Menu::set_focus(int direction) {
     }
 }
 
+#ifdef __linux__
+char getch_l() { // This solution was found at https://stackoverflow.com/questions/421860/capture-characters-from-standard-input-without-waiting-for-enter-to-be-pressed, by anon and Falcon Momot
+    char buf = 0;
+    struct termios old = {0};
+    if (tcgetattr(0, &old) < 0)
+        perror("tcsetattr()");
+    old.c_lflag &= ~ICANON;
+    old.c_lflag &= ~ECHO;
+    old.c_cc[VMIN] = 1;
+    old.c_cc[VTIME] = 0;
+    if (tcsetattr(0, TCSANOW, &old) < 0)
+        perror("tcsetattr ICANON");
+    if (read(0, &buf, 1) < 0)
+        perror("read()");
+    old.c_lflag |= ICANON;
+    old.c_lflag |= ECHO;
+    if (tcsetattr(0, TCSADRAIN, &old) < 0)
+        perror("tcsettattr ~ICANON");
+    return buf;
+}
+#endif
+
 int Menu::get_focus() { // TODO: Make Linux compatible version
         while (1) {
             #ifdef _WIN32
@@ -103,10 +130,23 @@ int Menu::get_focus() { // TODO: Make Linux compatible version
                     } else if (code == 13) { // This is the code for pressing the return key
                         return 0;
                     } else if (code == 113) { // This is the code for the q key, returning -1 will exit the current menu render loop
-                    return -1;
+                        return -1;
                     }
                 }
             #elif __linux__
+                int code = getch_l();
+                if (code == 27 && getch_l() == 91) { 
+                    switch (getch_l()) {
+                        case 65: // Code for up arrow key on linux (at the time of writing only tested on Arch Linux)
+                            return 1;
+                        case 66: // Code for the down arrow key on linux (at the time of writing only tested on Arch Linux)
+                            return 2;
+                    }
+                } else if (code == 10) { // This is the code for pressing the return key
+                    return 0;
+                } else if (code == 113) { // This is the code for pressing q
+                    return -1;
+                }
             #endif
         }
 }
